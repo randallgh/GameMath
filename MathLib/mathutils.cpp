70c249a8bd94f02174f539758fa207ba2b91d211
randallgh <randallhines0@gmail.com>
2017-10-10 09:30:25 -0700

    Move around files and add MathLib, add X-TestSpace

---
+++ MathLib/mathutils.cpp
@@ -0,0 +1,11 @@
+#include "mathutils.h"
+
+float min(float a, float b)
+{
+	return a < b ? a : b;
+}
+
+float max(float a, float b)
+{
+	return a > b ? a : b;
+}
---
+++ X-TestSpace/main.cpp
@@ -0,0 +1,7 @@
+#include "mathutils.h"
+
+int main() 
+{
+
+	return 0;
+}
