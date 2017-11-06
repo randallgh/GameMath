dd63a12b1dac8e116240e8e8bc3886a15a96b6d2
Randall Hines <randallhines0@gmail.com>
2017-11-06 11:09:13 -0800

    Setup Torpedo and TorpedoMount header files

--- A-Botes2D/Torpedo.h
+++ A-Botes2D/Torpedo.h
@@ -1,10 +1,38 @@
 #pragma once
 #include "GameObject.h"
-class Torpedo :
-	public GameObject
+#include "mat3x3.h"
+
+class TorpedoMount;
+
+class Torpedo : public GameObject
 {
 public:
 	Torpedo();
 	~Torpedo();
+
+	//Parent
+	TorpedoMount * parentMount;
+
+	//Varibles
+	float length;
+	float width;
+
+	float life;
+	float maxLife;
+
+	float distanceTraveled;
+	float maxDistance;
+
+	//Feet / Second 
+	float speed;
+	//Feet
+	float range;
+
+	//Methods
+
+	void reset();
+
+	void update(float dt);
+	void draw(mat3x3 cam);
 };
 
--- A-Botes2D/TorpedoMount.h
+++ A-Botes2D/TorpedoMount.h
@@ -1,10 +1,33 @@
 #pragma once
 #include "GameObject.h"
-class TorpedoMount :
-	public GameObject
+#include "vec2.h"
+#include "mat3x3.h"
+
+class Ship;
+class Torpedo;
+class Physics;
+
+class TorpedoMount : public GameObject
 {
 public:
 	TorpedoMount();
 	~TorpedoMount();
+
+	Ship * parentShip;
+	Torpedo * torpedo;
+	Physics * physics;
+
+	float reloadTime;
+	float reloadTimer;
+
+	int MAX_TORPEDOES = 20;
+	int numTorpedoes = 0;
+	Torpedo ** torpedos = new Torpedo*[MAX_TORPEDOES];
+
+	void update(float dt);
+	void draw(mat3x3 cam);
+
+	void launchNext(vec2 pos);
+	void launchAll(vec2 pos);
 };
 
